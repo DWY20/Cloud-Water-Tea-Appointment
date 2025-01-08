a7320f4befdbfe93826f6bfac9f3b02c914d3fcf
DWY20 <514643044@qq.com>
2025-01-08 21:51:22 +0800

    创建后端spring boot项目

---
+++ Cloud_Water_Tea_Appointment/src/main/java/com/dwy2002/cloud_water_tea_appointment/CloudWaterTeaAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.dwy2002.cloud_water_tea_appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWaterTeaAppointmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudWaterTeaAppointmentApplication.class, args);
+    }
+
+}
---
+++ Cloud_Water_Tea_Appointment/src/test/java/com/dwy2002/cloud_water_tea_appointment/CloudWaterTeaAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwy2002.cloud_water_tea_appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudWaterTeaAppointmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
